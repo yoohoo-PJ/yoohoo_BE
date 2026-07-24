d6cc7bd9ee6706eda3e9d6b2460740f6459b05e7
gojunbeom430-alt <gojunbeom430@gmail.com>
2026-07-24 14:51:41 +0900

    Chore: first Commit

---
+++ src/main/java/com/example/yoohoo_be/YoohooBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoohoo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoohooBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoohooBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yoohoo_be/YoohooBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yoohoo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoohooBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
